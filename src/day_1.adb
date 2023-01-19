bfeee670b8f02c290782b014793159347837e041
Georgios Kotzampopoulos <__alias__@tutanota.com>
2023-01-19 19:13:22 +0100

    Stylistic cleanups

--- src/day_1.adb
+++ src/day_1.adb
@@ -1,88 +1,92 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Containers.Vectors;
-with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
 
 with GNAT.AWK;
 
 procedure Day_1 is
-   subtype Calories is Natural;
+        subtype Calories is Natural;
 
-   type Top_3_Calories_Array is array (0 .. 2) of Calories;
-   type Top_3_Calories_Record is record
-      Top_3 : Top_3_Calories_Array := (others => 0);
-      Sum : Calories := 0;
-   end record;
+        type Top_3_Calories_Array is array (0 .. 2) of Calories;
+        type Top_3_Calories_Record is record
+                Top_3 : Top_3_Calories_Array := [others => 0];
+                Sum   : Calories             := 0;
+        end record;
 
-   -- Insertion sort
-   procedure Store_If_In_Top_3 (Self: in out Top_3_Calories_Record; C : in Calories) is
-   begin
-      for I in Self.Top_3'Range loop
-         if C > Self.Top_3 (I) then declare
-               -- Delta is what what comes - what leaves
-               Calories_Delta : constant Calories := C - Self.Top_3 (Self.Top_3'Last);
-            begin
-               Self.Sum := @ + Calories_Delta;
-               -- Shift 1 to the right
-               for J in reverse I + 1 .. Self.Top_3'Last loop
-                  Self.Top_3 (J) := Self.Top_3 (J - 1);
-               end loop;
-               Self.Top_3 (I) := C;
-               exit;
-            end;
-         end if;
-      end loop;
-   end Store_If_In_Top_3;
+        -- Insertion sort
+        procedure Store_If_In_Top_3
+               (Self : in out Top_3_Calories_Record; C : in Calories) is
+        begin
+                for I in Self.Top_3'Range loop
+                        if C > Self.Top_3 (I) then
+                                declare
+                                        -- Delta is what what comes - what leaves
+                                        Calories_Delta : constant Calories :=
+                                               C - Self.Top_3
+                                                      (Self.Top_3'Last);
+                                begin
+                                        Self.Sum := @ + Calories_Delta;
+                                        -- Shift 1 to the right
+                                        for J in reverse
+                                               I + 1 .. Self.Top_3'Last
+                                        loop
+                                                Self.Top_3 (J) :=
+                                                       Self.Top_3 (J - 1);
+                                        end loop;
+                                        Self.Top_3 (I) := C;
+                                        exit;
+                                end;
+                        end if;
+                end loop;
+        end Store_If_In_Top_3;
 
-   package Calories_Vectors is new
-     Ada.Containers.Vectors
-       (Index_Type => Natural,
-        Element_Type => Calories);
-   use Calories_Vectors;
+        package Calories_Vectors is new Ada.Containers.Vectors
+               (Index_Type => Natural, Element_Type => Calories);
+        use Calories_Vectors;
 
-   function Sum_Calories (V : in Calories_Vectors.Vector) return Calories is
-      Sum : Calories := 0;
-   begin
-      for C of V loop
-         Sum := @ + C;
-      end loop;
-      return Sum;
-   end Sum_Calories;
+        function Sum_Calories
+               (V : in Calories_Vectors.Vector) return Calories is
+                Sum : Calories := 0;
+        begin
+                for C of V loop
+                        Sum := @ + C;
+                end loop;
+                return Sum;
+        end Sum_Calories;
 
-   ------------------------------
-   -- Processing
-   ------------------------------
+        ------------------------------
+        -- Processing
+        ------------------------------
 
-   package AWK renames GNAT.AWK;
+        package AWK renames GNAT.AWK;
 
-   Top_3_Calories : Top_3_Calories_Record;
-   Calories_Vector : Calories_Vectors.Vector;
+        Top_3_Calories  : Top_3_Calories_Record;
+        Calories_Vector : Calories_Vectors.Vector;
 
-   procedure Action (Quit : in out Boolean) is
-      pragma Unreferenced (Quit);
+        procedure Action (Quit : in out Boolean) is
+                pragma Unreferenced (Quit);
 
-      Line : constant String := AWK.Field(0);
-   begin
-      if Line'Size /= 0 then
-         Calories_Vector.Append (Calories'Value (Line));
-     else declare
-            Sum : constant Calories := Sum_Calories (Calories_Vector);
-         begin
-            Put_Line ("Calories: " & Calories_Vector'Image & LF
-                      & "Sum:" & Sum'Image);
-            Store_If_In_Top_3 (Top_3_Calories, Sum);
-            Put_Line ("Top 3: " & Top_3_Calories'Image);
-            New_Line;
-            Calories_Vector.Clear;
-         end;
-      end if;
-   end Action;
+                Line : constant String := AWK.Field (0);
+        begin
+                if Line'Size /= 0 then
+                        Calories_Vector.Append (Calories'Value (Line));
+                else
+                        declare
+                                Sum : constant Calories :=
+                                       Sum_Calories (Calories_Vector);
+                        begin
+                                Store_If_In_Top_3 (Top_3_Calories, Sum);
+                                Calories_Vector.Clear;
+                        end;
+                end if;
+        end Action;
 
-   procedure Parse_Calories is new AWK.For_Every_Line_Current_Session (Action);
+        procedure Parse_Calories is new AWK.For_Every_Line_Current_Session
+               (Action);
 
 begin
-   Parse_Calories(Separators => AWK.Use_Current,
-                  Filename   => "input/day_1.txt");
+        Parse_Calories
+               (Separators => AWK.Use_Current, Filename => "input/day_1.txt");
 
-   New_Line;
-   Put_Line ("Finally Top_3_Calories: " & Top_3_Calories'Image);
+        New_Line;
+        Put_Line ("Finally Top_3_Calories: " & Top_3_Calories'Image);
 end Day_1;
--- src/day_2.adb
+++ src/day_2.adb
@@ -5,96 +5,89 @@ with GNAT.Regpat;
 
 procedure Day_2 is
 
-   package Choices is
-      -- Order of enums is crucial, do not change.
-      -- They all follow the pattern Rock-Paper-Scissors/Loss-Draw-Win when applicable.
+  package Choices is
+    -- Order of enums is crucial, do not change.
+    -- They all follow the pattern Rock-Paper-Scissors/Loss-Draw-Win when applicable.
 
-      type Opponent  is (A,    B,     C);
-      type Encrypted is (X,    Y,     Z);
-      type Pure      is (Rock, Paper, Scissors);
-      for Pure use (Rock => 1, Paper => 2, Scissors => 3);
-      function From_String (S : in String) return Pure;
+    type Opponent is (A, B, C);
+    type Encrypted is (X, Y, Z);
+    type Pure is (Rock, Paper, Scissors);
+    for Pure use (Rock => 1, Paper => 2, Scissors => 3);
+    function From_String (S : in String) return Pure;
 
-      type Outcome is (Loss, Draw, Win);
-      for Outcome use (Loss => 0, Draw => 3, Win => 6);
-      function From_String (S : in String) return Outcome;
+    type Outcome is (Loss, Draw, Win);
+    for Outcome use (Loss => 0, Draw => 3, Win => 6);
+    function From_String (S : in String) return Outcome;
 
-      function Cheat (Opponent : in Pure; Outcome : in Choices.Outcome) return Pure;
-   end Choices;
+    function Cheat
+     (Opponent : in Pure; Outcome : in Choices.Outcome) return Pure;
+  end Choices;
 
-   package body Choices is
-      function From_String (S : in String) return Outcome is
-         Encrypted     : constant Choices.Encrypted := Choices.Encrypted'Value (S);
-         Encrypted_Pos : constant Natural           := Choices.Encrypted'Pos (Encrypted);
-         Outcome       : constant Choices.Outcome   := Choices.Outcome'Val (Encrypted_Pos);
-      begin
-         return Outcome;
-      end From_String;
+  package body Choices is
+    function From_String (S : in String) return Outcome is
+      Encrypted : constant Choices.Encrypted := Choices.Encrypted'Value (S);
+      Encrypted_Pos : constant Natural := Choices.Encrypted'Pos (Encrypted);
+      Outcome       : constant Choices.Outcome   :=
+       Choices.Outcome'Val (Encrypted_Pos);
+    begin
+      return Outcome;
+    end From_String;
 
-      function From_String (S : in String) return Pure is
-         Opponent     : constant Choices.Opponent := Choices.Opponent'Value (S);
-         Opponent_Pos : constant Natural          := Choices.Opponent'Pos (Opponent) mod 3;
-         Choice       : constant Pure             := Choices.Pure'Val (Opponent_Pos);
-      begin
-         return Choice;
-      end From_String;
+    function From_String (S : in String) return Pure is
+      Opponent     : constant Choices.Opponent := Choices.Opponent'Value (S);
+      Opponent_Pos : constant Natural := Choices.Opponent'Pos (Opponent) mod 3;
+      Choice       : constant Pure := Choices.Pure'Val (Opponent_Pos);
+    begin
+      return Choice;
+    end From_String;
 
-      function Cheat (Opponent : in Pure; Outcome : in Choices.Outcome) return Pure is
-         Outcome_Choice_Offsets : constant array (Loss .. Win) of Integer := (-1, 0, 1);
-         ------------------
-         Opponent_Pos : constant Natural := Pure'Pos (Opponent);
-         Choice_Pos   : constant Natural := (Opponent_Pos + Outcome_Choice_Offsets(Outcome)) mod 3;
-      begin
-         return Choices.Pure'Val (Choice_Pos);
-      end Cheat;
-      -- The basic idea is for opponent choice C the player may:
-      -- * Win  with C - 1
-      -- * Draw with C + 0
-      -- * Loss with C + 1
-      -- Unfortunatelly we cannot map negative numbers in Ada enums, so we simulate it by
-      -- adding the Pos of the outcome to the C. Thus for Outcome O it becomes:
-      -- * Win  with (C + Win)  mod 3 = (C + 2) mod 3
-      -- * Draw with (C + Draw) mod 3 = (C + 0) mod 3
-      -- * Loss with (C + Loss) mod 3 = (C + 1) mod 3
-      -- Use of mod 3 is to prevent Constraint_Error and wrap the position around the enum.
-   end Choices;
+    function Cheat
+     (Opponent : in Pure; Outcome : in Choices.Outcome) return Pure is
+      Outcome_Choice_Offsets : constant array (Loss .. Win) of Integer :=
+       [-1, 0, 1];
+      ------------------
+      Opponent_Pos           : constant Natural := Pure'Pos (Opponent);
+      Choice_Pos             : constant Natural                        :=
+       (Opponent_Pos + Outcome_Choice_Offsets (Outcome)) mod 3;
+    begin
+      return Choices.Pure'Val (Choice_Pos);
+    end Cheat;
+    -- The basic idea is for opponent choice C the player may:
+    -- * Win  with C - 1
+    -- * Draw with C + 0
+    -- * Loss with C + 1
+    -- Unfortunatelly we cannot map negative numbers in Ada enums, so we simulate it by
+    -- adding the Pos of the outcome to the C. Thus for Outcome O it becomes:
+    -- * Win  with (C + Win)  mod 3 = (C + 2) mod 3
+    -- * Draw with (C + Draw) mod 3 = (C + 0) mod 3
+    -- * Loss with (C + Loss) mod 3 = (C + 1) mod 3
+    -- Use of mod 3 is to prevent Constraint_Error and wrap the position around the enum.
+  end Choices;
 
-   ------------------------------
-   -- Processing
-   ------------------------------
+  ------------------------------
+  -- Processing
+  ------------------------------
 
-   package AWK renames GNAT.AWK;
-   package Regpat renames GNAT.Regpat;
+  package AWK renames GNAT.AWK;
+  package Regpat renames GNAT.Regpat;
 
-   Total_Score : Natural := 0;
-   procedure Action is
-      type Round is record
-         Outcome             : Choices.Outcome;
-         Outcome_Value       : Natural;
-         Opponent, Player    : Choices.Pure;
-         Choice_Value, Score : Natural;
-      end record;
-      -- Just for the convenient 'Image
-
-      Opponent      : constant Choices.Pure    := Choices.From_String (AWK.Field(1));
-      Outcome       : constant Choices.Outcome := Choices.From_String (AWK.Field(2));
-      Outcome_Value : constant Natural         := Choices.Outcome'Enum_Rep (Outcome);
-      Player        : constant Choices.Pure    := Choices.Cheat (Opponent, Outcome);
-      Choice_Value  : constant Natural         := Choices.Pure'Enum_Rep (Player);
-      Score         : constant Natural         := Choice_Value + Outcome_Value;
-
-      R : constant Round := (Outcome, Outcome_Value, Opponent, Player, Choice_Value, Score);
-   begin
-      Total_Score := @ + Score;
-      Put_Line (R'Image & Total_Score'Image);
-   end Action;
+  Total_Score : Natural := 0;
+  procedure Action is
+    Opponent : constant Choices.Pure    := Choices.From_String (AWK.Field (1));
+    Outcome : constant Choices.Outcome := Choices.From_String (AWK.Field (2));
+    Outcome_Value : constant Natural := Choices.Outcome'Enum_Rep (Outcome);
+    Player        : constant Choices.Pure := Choices.Cheat (Opponent, Outcome);
+    Choice_Value  : constant Natural         := Choices.Pure'Enum_Rep (Player);
+    Score         : constant Natural         := Choice_Value + Outcome_Value;
+  begin
+    Total_Score := @ + Score;
+  end Action;
 
 begin
-   AWK.Register(Field => 0,
-                Pattern => Regpat.Compile("[A-C] [X-Z]"),
-                Action => Action'Unrestricted_Access
-               );
-   AWK.Parse(AWK.Use_Current, "input/day_2.txt");
+  AWK.Register
+   (Field  => 0, Pattern => Regpat.Compile ("[A-C] [X-Z]"),
+    Action => Action'Unrestricted_Access);
+  AWK.Parse (AWK.Use_Current, "input/day_2.txt");
 
-   Put_Line ("Final score: " & Total_Score'Image);
+  Put_Line ("Final score: " & Total_Score'Image);
 end Day_2;
